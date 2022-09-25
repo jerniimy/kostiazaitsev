83ec8ee544b0f9640c2072862453e67e7030ca89
jerniimy <jerniimy@gmail.com>
2022-09-25 20:48:22 +0300

    feat: add modal window and info to purpose page

---
+++ main.js
@@ -0,0 +1,9 @@
+const modal = document.getElementById('wrapper');
+const closeButton = document.getElementById('close');
+
+function modalWindow() {
+  modal.classList.add('active');
+};
+function closeWindow() {
+  modal.classList.remove('active');
+};
