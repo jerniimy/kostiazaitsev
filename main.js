7bd2eb7ebd46140f51d0714335ad8cf0c96b2eb9
jerniimy <jerniimy@gmail.com>
2022-09-30 21:21:32 +0300

    feat: backgroumd om hover to vald page and change purpose main page

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ function modalWindow() {
 function closeWindow() {
   modal.classList.remove('active');
 };
+
+// vald
+
+document.getElementById('VALD').addEventListener('mouseover', (e) => {
+  document.getElementById('VALDBackground').classList.remove('active');
+  document.getElementById('VALDBackgroundHover').classList.add('active');
+});
+document.getElementById('VALD').addEventListener('mouseout', (e) => {
+  document.getElementById('VALDBackgroundHover').classList.remove('active');
+  document.getElementById('VALDBackground').classList.add('active');
+});
