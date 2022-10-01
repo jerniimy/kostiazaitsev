617465e906ea269baa554ee98ebd95c736acdc8f
jerniimy <jerniimy@gmail.com>
2022-10-01 15:11:31 +0300

    feat: add purpose gif

--- main.js
+++ main.js
@@ -29,5 +29,5 @@ window.onload = function () {
   window.setTimeout(function () {
     document.body.classList.add('loaded');
     document.body.classList.remove('loaded_hiding');
-  }, 500);
+  }, 100);
 }
